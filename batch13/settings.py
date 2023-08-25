ad0ebdd2e5f625b144862ec2393c594f328dfd36
dokideekshitha <142998418+dokideekshitha@users.noreply.github.com>
2023-08-25 19:39:58 +0530

    Update settings.py

--- batch13/settings.py
+++ batch13/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-umeicua15kszq$$uzp&l&_31h-l4l@uo(3#%^+8kl^d9ayhr+!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['43.205.128.128','localhost','8000']
+ALLOWED_HOSTS = ['43.205.128.128','localhost','0.0.0.0']
 
 
 # Application definition
